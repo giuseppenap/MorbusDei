5e8ba9e93534efccde78948633f2f29de99559f3
Giuseppe <napoligiuseppe@web.de>
2026-04-14 13:29:34 +0200

    First Push / Repo Setup

---
+++ Source/MorbusDei/MorbusDei.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MorbusDei.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MorbusDei, "MorbusDei" );
